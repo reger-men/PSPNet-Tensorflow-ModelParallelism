279285d4e9ea9741d7208a0e4c37d3ecf42d55c7
reger men <adil-lashab@hotmail.com>
2018-10-09 14:58:07 +0000

    small example

--- train.py
+++ train.py
@@ -1,221 +1,198 @@
-"""
-This code is based on DrSleep's framework: https://github.com/DrSleep/tensorflow-deeplab-resnet 
-"""
-
-from __future__ import print_function
-
-import argparse
-import os
-import sys
-import time
-
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.memory_stats.python.ops.memory_stats_ops import BytesLimit, BytesInUse, MaxBytesInUse
+from time import time
+import math
 
-from model import PSPNet101, PSPNet50
-from tools import prepare_label
-from image_reader import ImageReader
-
-IMG_MEAN = np.array((103.939, 116.779, 123.68), dtype=np.float32)
-
-BATCH_SIZE = 2
-DATA_DIRECTORY = '/SSD_data/cityscapes_dataset/cityscape'
-DATA_LIST_PATH = './list/cityscapes_train_list.txt'
-IGNORE_LABEL = 255
-INPUT_SIZE = '713,713'
-LEARNING_RATE = 1e-3
-MOMENTUM = 0.9
-NUM_CLASSES = 19
-NUM_STEPS = 60001
-POWER = 0.9
-RANDOM_SEED = 1234
-WEIGHT_DECAY = 0.0001
-RESTORE_FROM = './'
-SNAPSHOT_DIR = './model/'
-SAVE_NUM_IMAGES = 4
-SAVE_PRED_EVERY = 50
+from include.data import get_data_set, resize_data
+from include.model import s_model, lr
 
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description="DeepLab-ResNet Network")
-    parser.add_argument("--batch-size", type=int, default=BATCH_SIZE,
-                        help="Number of images sent to the network in one step.")
-    parser.add_argument("--data-dir", type=str, default=DATA_DIRECTORY,
-                        help="Path to the directory containing the PASCAL VOC dataset.")
-    parser.add_argument("--data-list", type=str, default=DATA_LIST_PATH,
-                        help="Path to the file listing the images in the dataset.")
-    parser.add_argument("--ignore-label", type=int, default=IGNORE_LABEL,
-                        help="The index of the label to ignore during the training.")
-    parser.add_argument("--input-size", type=str, default=INPUT_SIZE,
-                        help="Comma-separated string with height and width of images.")
-    parser.add_argument("--is-training", action="store_true",
-                        help="Whether to updates the running means and variances during the training.")
-    parser.add_argument("--learning-rate", type=float, default=LEARNING_RATE,
-                        help="Base learning rate for training with polynomial decay.")
-    parser.add_argument("--momentum", type=float, default=MOMENTUM,
-                        help="Momentum component of the optimiser.")
-    parser.add_argument("--not-restore-last", action="store_true",
-                        help="Whether to not restore last (FC) layers.")
-    parser.add_argument("--num-classes", type=int, default=NUM_CLASSES,
-                        help="Number of classes to predict (including background).")
-    parser.add_argument("--num-steps", type=int, default=NUM_STEPS,
-                        help="Number of training steps.")
-    parser.add_argument("--power", type=float, default=POWER,
-                        help="Decay parameter to compute the learning rate.")
-    parser.add_argument("--random-mirror", action="store_true",
-                        help="Whether to randomly mirror the inputs during the training.")
-    parser.add_argument("--random-scale", action="store_true",
-                        help="Whether to randomly scale the inputs during the training.")
-    parser.add_argument("--random-seed", type=int, default=RANDOM_SEED,
-                        help="Random seed to have reproducible results.")
-    parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
-                        help="Where restore model parameters from.")
-    parser.add_argument("--save-num-images", type=int, default=SAVE_NUM_IMAGES,
-                        help="How many images to save.")
-    parser.add_argument("--save-pred-every", type=int, default=SAVE_PRED_EVERY,
-                        help="Save summaries and checkpoint every often.")
-    parser.add_argument("--snapshot-dir", type=str, default=SNAPSHOT_DIR,
-                        help="Where to save snapshots of the model.")
-    parser.add_argument("--weight-decay", type=float, default=WEIGHT_DECAY,
-                        help="Regularisation parameter for L2-loss.")
-    parser.add_argument("--update-mean-var", action="store_true",
-                        help="whether to get update_op from tf.Graphic_Keys")
-    parser.add_argument("--train-beta-gamma", action="store_true",
-                        help="whether to train beta & gamma in bn layer")
-    return parser.parse_args()
+#Params
+global_accuracy = 0
+global_avg_bs = 0
+global_counter = 0
+global_model = "small"
 
-def save(saver, sess, logdir, step):
-   model_name = 'model.ckpt'
-   checkpoint_path = os.path.join(logdir, model_name)
-    
-   if not os.path.exists(logdir):
-      os.makedirs(logdir)
-   saver.save(sess, checkpoint_path, global_step=step)
-   print('The checkpoint has been created.')
+_batch_size = 10 #Max Batch_Size=14895
+_epoch = 10
 
-def load(saver, sess, ckpt_path):
-    saver.restore(sess, ckpt_path)
-    print("Restored model parameters from {}".format(ckpt_path))
+
+#parse arguments
+import optparse
+parser = optparse.OptionParser()
+parser.add_option('-m', '--model', type=str , dest="model", default="small", help="define whatever model should be run (small or big)")
+parser.add_option('-i', '--input_size', type=int , dest="input_size", default=32, help="define the Input size as integer. Default value is 32")
+parser.add_option('-b', '--batch_size', type=int , dest="batch_size", default=128, help="define the batch size. Default value is 128")
+parser.add_option('-e', '--epoch', type=int , dest="epoch", default=10, help="define the Epoch number. Default value is 10")
+parser.add_option('-g', '--gpu_array', type=str, dest="gpu_array", default='0,1,1', help="define on which GPUs should the model be splited. Default value is '0,1,1'")
+
+options, args = parser.parse_args()
+global_model = options.model
+_batch_size = options.batch_size
+_epoch = options.epoch
+_input_size = options.input_size
+
+train_x, train_y = get_data_set("train")
+test_x, test_y = get_data_set("test")
+
+#Resize the dataset
+_train_size = train_x.shape[0]
+_test_size = test_x.shape[0]
+train_x = resize_data(train_x, _input_size, _input_size, _train_size)
+train_y = train_y[:_train_size, :]
+
+test_x = resize_data(test_x, _input_size, _input_size, _test_size)
+test_y = test_y[:_test_size, :]
+
+#Set Model to use
+if(global_model == "small"):
+    x, y, output, y_pred_cls, global_step, learning_rate = s_model()
+
+#PARAMS
+_SAVE_PATH = "./tensorboard/cifar-10-v1.0.0/"
+
+
+# LOSS AND OPTIMIZER
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                   beta1=0.9,
+                                   beta2=0.999,
+                                   epsilon=1e-08).minimize(loss, global_step=global_step)
+
+
+# PREDICTION AND ACCURACY CALCULATION
+correct_prediction = tf.equal(y_pred_cls, tf.argmax(y, axis=1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# SAVER
+merged = tf.summary.merge_all()
+saver = tf.train.Saver()
+session_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+session_config.gpu_options.allow_growth = True
+sess = tf.Session(config=session_config)
+train_writer = tf.summary.FileWriter(_SAVE_PATH, sess.graph)
+
+
+'''try:
+    print("\nTrying to restore last checkpoint ...")
+    last_chk_path = tf.train.latest_checkpoint(checkpoint_dir=_SAVE_PATH)
+    saver.restore(sess, save_path=last_chk_path)
+    print("Restored checkpoint from:", last_chk_path)
+except ValueError:
+    print("\nFailed to restore checkpoint. Initializing variables instead.")
+    sess.run(tf.global_variables_initializer())
+'''
+sess.run(tf.global_variables_initializer())
+
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
+
+def train(epoch):
+    #run_opts = tf.RunOptions(report_tensor_allocations_upon_oom=True)
+    batch_size = int(math.ceil(_train_size / _batch_size))
+    i_global = 0
+    global global_avg_bs 
+    global global_counter
+
+    for s in range(batch_size):
+        batch_xs = train_x[s*_batch_size: (s+1)*_batch_size]
+        batch_ys = train_y[s*_batch_size: (s+1)*_batch_size]
+
+        start_time = time()
+        i_global, _, batch_loss, batch_acc = sess.run(
+            [global_step, optimizer, loss, accuracy],
+            feed_dict={x: batch_xs, y: batch_ys, learning_rate: lr(epoch)})
+        duration = time() - start_time
+
+        if s % 100 == 0:
+            percentage = int(round((s/batch_size)*100))
+            #print(percentage)
+            bar_len = 29
+            filled_len = int((bar_len*int(percentage))/100)
+            bar = '=' * filled_len + '>' + '-' * (bar_len - filled_len)
+
+            msg = "Global step: {:>5} - [{}] {:>3}% - acc: {:.4f} - loss: {:.4f} - {:.1f} sample/sec"
+            print(msg.format(i_global, bar, percentage, batch_acc, batch_loss, _batch_size / duration))
+            global_avg_bs += (_batch_size / duration)
+            global_counter += 1 
+
+    #Print GPUs usage
+    '''with tf.device('/device:GPU:0'):
+        bytes_in_use = MaxBytesInUse()
+        print("memory usage on GPU0: ", convert_size(sess.run(bytes_in_use)))
+    with tf.device('/device:GPU:1'):
+        bytes_in_use = MaxBytesInUse()
+        print("memory usage on GPU1: ", convert_size(sess.run(bytes_in_use)))
+    with tf.device('/device:GPU:2'):
+        bytes_in_use = MaxBytesInUse()
+        print("memory usage on GPU2: ", convert_size(sess.run(bytes_in_use)))
+    with tf.device('/device:GPU:3'):
+        bytes_in_use = MaxBytesInUse()
+        print("memory usage on GPU3: ", convert_size(sess.run(bytes_in_use)))
+    '''
+
+    test_and_save(i_global, epoch)
+
+
+def test_and_save(_global_step, epoch):
+    global global_accuracy
+
+    i = 0
+    predicted_class = np.zeros(shape=_test_size, dtype=np.int)
+    while i < _test_size:
+        j = min(i + _batch_size, _test_size)
+        batch_xs = test_x[i:j, :]
+        batch_ys = test_y[i:j, :]
+        predicted_class[i:j] = sess.run(
+            y_pred_cls,
+            feed_dict={x: batch_xs, y: batch_ys, learning_rate: lr(epoch)}
+        )
+        i = j
+
+    correct = (np.argmax(test_y, axis=1) == predicted_class)
+    acc = correct.mean()*100
+    correct_numbers = correct.sum()
+
+    mes = "\nEpoch {} - accuracy: {:.2f}% ({}/{})"
+    print(mes.format((epoch+1), acc, correct_numbers, _test_size))
+
+    if global_accuracy != 0 and global_accuracy < acc:
+
+        summary = tf.Summary(value=[
+            tf.Summary.Value(tag="Accuracy/test", simple_value=acc),
+        ])
+        train_writer.add_summary(summary, _global_step)
+
+        saver.save(sess, save_path=_SAVE_PATH, global_step=_global_step)
+
+        mes = "This epoch receive better accuracy: {:.2f} > {:.2f}. Saving session..."
+        print(mes.format(acc, global_accuracy))
+        global_accuracy = acc
+
+    elif global_accuracy == 0:
+        global_accuracy = acc
+
+    print("###########################################################################################################")
+
 
 def main():
-    """Create the model and start the training."""
-    args = get_arguments()
-    
-    h, w = map(int, args.input_size.split(','))
-    input_size = (h, w)
-    
-    tf.set_random_seed(args.random_seed)
-    
-    coord = tf.train.Coordinator()
-    
-    with tf.name_scope("create_inputs"):
-        reader = ImageReader(
-            args.data_dir,
-            args.data_list,
-            input_size,
-            args.random_scale,
-            args.random_mirror,
-            args.ignore_label,
-            IMG_MEAN,
-            coord)
-        image_batch, label_batch = reader.dequeue(args.batch_size)
-    
-    net = PSPNet101({'data': image_batch}, is_training=True, num_classes=args.num_classes)
-    
-    raw_output = net.layers['conv6']
+    for i in range(_epoch):
+        print("\nEpoch: {0}/{1}\n".format((i+1), _epoch))
+        train(i)
+    print("AVG sample/sec: ", global_avg_bs/global_counter)
 
-    with tf.device('/gpu:0'):
-        # According from the prototxt in Caffe implement, learning rate must multiply by 10.0 in pyramid module
-        fc_list = ['conv5_3_pool1_conv', 'conv5_3_pool2_conv', 'conv5_3_pool3_conv', 'conv5_3_pool6_conv', 'conv6', 'conv5_4']
-        restore_var = [v for v in tf.global_variables()]
-        all_trainable = [v for v in tf.trainable_variables() if ('beta' not in v.name and 'gamma' not in v.name) or args.train_beta_gamma]
-        fc_trainable = [v for v in all_trainable if v.name.split('/')[0] in fc_list]
-        conv_trainable = [v for v in all_trainable if v.name.split('/')[0] not in fc_list] # lr * 1.0
-        fc_w_trainable = [v for v in fc_trainable if 'weights' in v.name] # lr * 10.0
-        fc_b_trainable = [v for v in fc_trainable if 'biases' in v.name] # lr * 20.0
-        assert(len(all_trainable) == len(fc_trainable) + len(conv_trainable))
-        assert(len(fc_trainable) == len(fc_w_trainable) + len(fc_b_trainable))
-    
-        # Predictions: ignoring all predictions with labels greater or equal than n_classes
-        raw_prediction = tf.reshape(raw_output, [-1, args.num_classes])
-        label_proc = prepare_label(label_batch, tf.stack(raw_output.get_shape()[1:3]), num_classes=args.num_classes, one_hot=False) # [batch_size, h, w]
-        raw_gt = tf.reshape(label_proc, [-1,])
-        indices = tf.squeeze(tf.where(tf.less_equal(raw_gt, args.num_classes - 1)), 1)
-        gt = tf.cast(tf.gather(raw_gt, indices), tf.int32)
-        prediction = tf.gather(raw_prediction, indices)
-                                                                                            
-        # Pixel-wise softmax loss.
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=prediction, labels=gt)
-        l2_losses = [args.weight_decay * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'weights' in v.name]
-        reduced_loss = tf.reduce_mean(loss) + tf.add_n(l2_losses)
-
-        # Using Poly learning rate policy 
-        base_lr = tf.constant(args.learning_rate)
-        step_ph = tf.placeholder(dtype=tf.float32, shape=())
-        learning_rate = tf.scalar_mul(base_lr, tf.pow((1 - step_ph / args.num_steps), args.power))
-    
-    # Gets moving_mean and moving_variance update operations from tf.GraphKeys.UPDATE_OPS
-    if args.update_mean_var == False:
-        update_ops = None
-    else:
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-
-    with tf.control_dependencies(update_ops):
-        opt_conv = tf.train.MomentumOptimizer(learning_rate, args.momentum)
-        opt_fc_w = tf.train.MomentumOptimizer(learning_rate * 10.0, args.momentum)
-        opt_fc_b = tf.train.MomentumOptimizer(learning_rate * 20.0, args.momentum)
-
-        grads = tf.gradients(reduced_loss, conv_trainable + fc_w_trainable + fc_b_trainable)
-        grads_conv = grads[:len(conv_trainable)]
-        grads_fc_w = grads[len(conv_trainable) : (len(conv_trainable) + len(fc_w_trainable))]
-        grads_fc_b = grads[(len(conv_trainable) + len(fc_w_trainable)):]
-
-        train_op_conv = opt_conv.apply_gradients(zip(grads_conv, conv_trainable))
-        train_op_fc_w = opt_fc_w.apply_gradients(zip(grads_fc_w, fc_w_trainable))
-        train_op_fc_b = opt_fc_b.apply_gradients(zip(grads_fc_b, fc_b_trainable))
-
-        train_op = tf.group(train_op_conv, train_op_fc_w, train_op_fc_b)
-        
-    # Set up tf session and initialize variables. 
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.allow_soft_placement=True
-    config.gpu_options.allocator_type = 'BFC'
-    sess = tf.Session(config=config)
-    init = tf.global_variables_initializer()
-    
-    sess.run(init)
-    
-    # Saver for storing checkpoints of the model.
-    saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=10)
-
-    ckpt = tf.train.get_checkpoint_state(SNAPSHOT_DIR)
-    if ckpt and ckpt.model_checkpoint_path:
-        loader = tf.train.Saver(var_list=restore_var)
-        load_step = int(os.path.basename(ckpt.model_checkpoint_path).split('-')[1])
-        load(loader, sess, ckpt.model_checkpoint_path)
-    else:
-        print('No checkpoint file found.')
-        load_step = 0
-
-    # Start queue threads.
-    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
-
-    # Iterate over training steps.
-    for step in range(args.num_steps):
-        start_time = time.time()
-        
-        feed_dict = {step_ph: step}
-        if step % args.save_pred_every == 0:
-            loss_value, _ = sess.run([reduced_loss, train_op], feed_dict=feed_dict)
-            save(saver, sess, args.snapshot_dir, step)
-        else:
-            loss_value, _ = sess.run([reduced_loss, train_op], feed_dict=feed_dict)
-        duration = time.time() - start_time
-        print('step {:d} \t loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
-        
-    coord.request_stop()
-    coord.join(threads)
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+
+sess.close()
